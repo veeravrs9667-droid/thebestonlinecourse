3a1e819d8935cc8a643afa42a60a901df114b37c
veeravrs9667-droid <veeravrs9667@gmail.com>
2026-01-03 21:16:01 +0530

    Update CorsConfig.java

--- src/main/java/com/example/ocm/security/CorsConfig.java
+++ src/main/java/com/example/ocm/security/CorsConfig.java
@@ -1,24 +1,20 @@
+
+
 package com.example.ocm.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class CorsConfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://thebestonlinecourse.netlify.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://thebestonlinecourse.netlify.app")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .exposedHeaders("Authorization");
     }
 }
-
