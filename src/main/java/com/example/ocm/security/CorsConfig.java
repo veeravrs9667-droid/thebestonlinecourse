d8fa32992d6ecbf1ae34d7498854b9ab5b61e81c
veeravrs9667-droid <veeravrs9667@gmail.com>
2026-01-03 20:19:51 +0530

    Update CorsConfig.java

--- src/main/java/com/example/ocm/security/CorsConfig.java
+++ src/main/java/com/example/ocm/security/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("https://thebestonlinecourse.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
